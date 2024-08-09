4684b63b2764aca88dfd0cdba72edceb3b87cd94
sunyonghong <syh@sunyonghong.com>
2024-08-09 14:22:56 +0800

    优化: 路由地址

--- src/PaymentService.php
+++ src/PaymentService.php
@@ -11,14 +11,14 @@ class PaymentService extends Service
     {
         $this->registerRoutes(function () {
             /** 接口路由 */
-            Route::group('system/package/payment', function () {
+            Route::group('admin/system/package/payment', function () {
                 // 支付列表
                 Route::get('/list', 'index');
                 // 配置页面
                 Route::get('/<gateway>/config', 'config')->pattern(['gateway' => '[a-zA-Z_]+']);
                 // 启用配置
                 Route::post('/<gateway>/status', 'changeStatus')->pattern(['gateway' => '[a-zA-Z_]+']);
-            })->prefix('\eduline\payment\admin\service\Config@')->middleware(['adminRoute']);
+            })->prefix('\eduline\payment\admin\service\Config@')->middleware(['adminRoute', 'init', 'bindLoginUser']);
         });
     }
 }
